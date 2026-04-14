fd1adf3764b7500087dd69c7829d529961a0877e
stockfishZ <rafasatria2512@gmail.com>
2026-04-14 08:21:11 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,28 @@
+//RAFA SATRIA PRATAMA - 13224074
+//SOAL 1 - MENARA LAMPU
+//14/04/2026
+
 #include <stdio.h>
 
-int main(void) {
+int main(){
+    int a;
+    
+    scanf("%d", &a);
+    printf("\n");
+
+    for (int i = 1; i < a+1; i++){
+        if (i % 2 == 0 && i % 3 == 0){
+            printf("BIRU\n");
+        }else if (i % 3 == 0){
+            printf("MERAH\n");
+        }else if (i % 2 == 0){
+            printf("KUNING\n");
+        }else if (i >= 10 && i % 2 == 0){
+            printf("HIJAU\n");
+        }else{
+            printf("%d\n", i);
+        }
+    }
+
     return 0;
 }
